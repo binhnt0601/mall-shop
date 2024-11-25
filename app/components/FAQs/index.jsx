dc67c499f515815dd2bc59d8eba20a52aec8d239
loctran <missu1123@gmail.com>
2024-11-26 00:37:00 +0700

    QA + roadmap gan xong

--- app/components/FAQs/index.jsx
+++ app/components/FAQs/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Typography from '@mui/material/Typography';
+import { Stack } from '@mui/material';
+import { Box } from '@mui/material';
 
 export default function FAQS() {
     const headline = (
@@ -9,34 +11,77 @@ export default function FAQS() {
         </Typography>
     );
 
+    const data = [
+        {
+            question: 'Q: What is AiMalls?',
+            answer:
+                "A: AiMalls is an AI powered online mall or store, a platform and an app that uses intelligent algorithms to provide personalized shopping experiences.",
+        },
+        {
+            question: 'Q: What are the benefits will AiMalls bring?',
+            answer:
+                "A: Personalized product recommendations, Chatbots for Customer Service, Automated Product Categorization, Mobile-Friendly design, Secure payment processing, Integration with Quality brands and dispute resolution.",
+        },
+        {
+            question: 'Q: What advantages AiMalls has against its competitors?',
+            answer:
+                "A: Security, fast and reliable shipping for customers, AiMalls algorithms can help online stores/apps manage their inventory more efficiently, with AI-powered chatbots and virtual assistants it can provide good customer service 24/7, AiMalls offers competitive pricing and affiliate programs.",
+        },
+        {
+            question: 'Q: When is AiMalls Launching live?',
+            answer:
+                'A: AiMalls is Launching in October.',
+        },
+        {
+            question: 'Q: How can I participate in the AiMalls Launch?',
+            answer:
+                "A: Please wait further announcement regarding Public Sale and Exchange Listing Schedule (TBA).",
+        },
+        {
+            question: 'Q: Do you have the project Whitepaper?',
+            answer:
+                "A: Yes, here is the link : https://cdn.aimalls.app/whitepaper.pdf",
+        },
+        {
+            question: 'Q: What is the ticker for AiMalls token?',
+            answer:
+                "A: It is $AIT",
+        },
+        {
+            question: 'Q: What is $AIT use cases?',
+            answer:
+                "A: With AIT you can buy sponsored slots to promote your store and products in our marketplace , also means of accessing exclusive content or products on the app, offering added value for users who hold the token.",
+        },
+        {
+            question: 'Q: What is the Total Token Supply?',
+            answer:
+                "A: 850,000",
+        },
+        {
+            question: 'Q: Where can I find the roadmap, token allocation, and token utility?',
+            answer:
+                "A: All can be found in our Whitepaper link provided above.",
+        },
+    ];
+
     return (
-        <div
-            padding="5px"
-            className="w-full flex flex-col items-center gap-5 py-10"
-        >
-            {headline}
-            <Typography>
-                Q: What is AiMalls?
-                A: AiMalls is an AI powered online mall or store, a platform and an app that uses intelligent algorithms to provide personalized shopping experiences.
-                Q: What are the benefits will AiMalls bring?
-                A: Personalized product recommendations, Chatbots for Customer Service, Automated Product Categorization, Mobile-Friendly design, Secure payment processing, Integration with Quality brands and dispute resolution.
-                Q: What advantages AiMalls has against its competitors?
-                A: Security, fast and reliable shipping for customers, AiMalls algorithms can help online stores/apps manage their inventory more efficiently, with AI-powered chatbots and virtual assistants it can provide good customer service 24/7, AiMalls offers competitive pricing and affiliate programs.
-                Q: When is AiMalls Launching live?
-                A: AiMalls is Launching in October.
-                Q: How can I participate in the AiMalls Launch?
-                A: Please wait further announcement regarding Public Sale and Exchange Listing Schedule (TBA).
-                Q: Do you have the project Whitepaper?
-                A: Yes, here is the link : https://cdn.aimalls.app/whitepaper.pdf
-                Q: What is the ticker for AiMalls token?
-                A: It is $AIT
-                Q: What is $AIT use cases?
-                A: With AIT you can buy sponsored slots to promote your store and products in our marketplace , also means of accessing exclusive content or products on the app, offering added value for users who hold the token.
-                Q: What is the Total Token Supply?
-                A: 850,000
-                Q: Where can I find the roadmap, token allocation, and token utility?
-                A: All can be found in our Whitepaper link provided above.
-            </Typography>
-        </div>
+        <Stack>
+            <div
+                padding="5px"
+                className="w-full flex flex-col items-center gap-5 py-10"
+            >
+                {headline}
+            </div>
+
+            <div>
+                {data.map((item) => (
+                    <Typography
+                        style={{ color: '#fc9a14', fontFamily: 'Work Sans', fontWeight: 'bold' }}>
+                        {item.question}
+                        {item.answer}
+                    </Typography>
+                ))}
+            </div>
+        </Stack>
     );
 }
