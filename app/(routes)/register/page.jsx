af4572601d9ffbfe40032a7319ccbdd6a3d437ae
Bee Nguyen <loctran131195@gmail.com>
2024-11-27 13:48:42 +0700

    update

--- app/(routes)/register/page.jsx
+++ app/(routes)/register/page.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-import { Stack, Typography } from "@mui/material";
+import { Stack, TextField, Typography } from "@mui/material";
 import Link from "next/link";
 
 import LogoCompany from "@/components/TopBar/LogoCompany";
@@ -11,53 +11,50 @@ export const dynamic = "force-dynamic";
 
 const RegisterPage = () => {
   return (
-    <Stack className="flex">
-      <div className="login-container">
-        <h2>Login</h2>
-        <form className="login-form">
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-
-          <button style={{ border: "1" }} type="submit">
-            Login
+    <Stack className="!flex-row w-full h-dvh">
+      <div className="md:w-[65%] w-full bg-black flex flex-col justify-center items-center">
+        <Typography fontSize={40} fontWeight="bold" color="#fc9a14">
+          Sign up to add an Account
+        </Typography>
+        <div className="max-w-[460px]">
+          <button className="bg-white w-full py-3 rounded-lg">
+            Sign in With Google
           </button>
-          <p>
-            Don't have an account? <a href="/signup">Sign up</a>
-          </p>
-        </form>
+
+          <div>
+            <TextField
+              variant="outlined"
+              label="Email"
+              className="bg-white rounded-lg w-full"
+            />
+            <TextField
+              variant="outlined"
+              label="Password"
+              className="bg-white rounded-lg w-full"
+            />
+            <TextField
+              variant="outlined"
+              label="Confirm Password"
+              className="bg-white rounded-lg w-full"
+            />
+          </div>
+        </div>
       </div>
       <div
-        className="flex flex-col justify-center items-center text-center 
-      p-5 w-[30%] bg-[#fc9a14] text-white"
+        className="md:flex hidden flex-col gap-5 justify-center items-center text-center 
+      p-5 w-[35%] bg-[#fc9a14] text-white"
       >
-        <LogoCompany />
-        <Typography>Welcome Back!</Typography>
+        <LogoCompany width={181} height={124} />
+        <Typography fontSize={40} fontWeight="bold">
+          Welcome Back!
+        </Typography>
         <Typography>
           Rediscover the wonders of AI malls. Log in today and immerse yourself
           in a world of seamless shopping, intelligent recommendations, and
           delightful surprises. Your AI-powered retail adventure awaits!
         </Typography>
         <Link
-          className="bg-[white] text-[black] text-[20px] w-full py-3 rounded-full"
+          className="bg-[white] text-[black] text-[20px] w-full py-3 rounded-full mt-5"
           href="/login"
         >
           Login
