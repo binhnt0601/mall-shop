d7ac1c6bf511615a08ed0cfac19a81c17bedb942
loctran <missu1123@gmail.com>
2024-11-27 14:29:12 +0700

    login

--- app/(routes)/login/page.jsx
+++ app/(routes)/login/page.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography, TextField } from "@mui/material";
 import Link from "next/link";
 
 import LogoCompany from "@/components/TopBar/LogoCompany";
@@ -10,62 +10,55 @@ import LogoCompany from "@/components/TopBar/LogoCompany";
 export const dynamic = "force-dynamic";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPasswordl] = useState("");
-  const [error, setError] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPasswordl] = useState("");
+  // const [error, setError] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (!email || !password) {
-      setError("Email and Password can not be empty");
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   if (!email || !password) {
+  //     setError("Email and Password can not be empty");
 
-      return;
-    }
-    if (email === "user@example.com" && password === "password123") {
-      setError("");
-      alert("Login successful!");
-    } else {
-      setError("Invalid email or password.");
-    }
-  };
+  //     return;
+  //   }
+  //   if (email === "user@example.com" && password === "password123") {
+  //     setError("");
+  //     alert("Login successful!");
+  //   } else {
+  //     setError("Invalid email or password.");
+  //   }
+  // };
 
   return (
     <Stack className="!flex-row w-full h-dvh">
-      <div className="w-full">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit} className="login-form">
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            {error && <div className="error-message">{error}</div>}
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter your email"
-              required
-            />
-          </div>
+      <div className="md:w-[65%] w-full bg-black flex flex-col justify-center items-center ">
+        <Typography fontSize={40} fontWeight="bold" color="#fc9a14">
+          Welcome back
+        </Typography>
+        <div className="max-w-[460px]">
 
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-
-          <button style={{ border: "1" }} type="submit">
-            Login
+          <button className="bg-white w-full py-3 rounded-lg">
+            Sign in With Google
           </button>
-        </form>
+
+          <div>
+            <TextField
+              variant="outlined"
+              label="Email"
+              className="bg-white rounded-lg w-full"
+            />
+            <TextField
+              variant="outlined"
+              label="Password"
+              className="bg-white rounded-lg w-full"
+            />
+            <button className="bg-white rounded-lg w-full">
+              Login
+            </button>
+          </div>
+        </div>
       </div>
-      <div
-        className="md:flex hidden flex-col gap-5 justify-center items-center text-center 
+      <div className="md:flex hidden flex-col gap-5 justify-center items-center text-center 
       p-5 w-[50%] bg-[#fc9a14] text-white"
       >
         <LogoCompany width={181} height={124} />
@@ -85,6 +78,7 @@ const LoginPage = () => {
           Register
         </Link>
       </div>
+
     </Stack>
   );
 };
