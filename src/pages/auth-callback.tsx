955865d296a948cd51b5ab60ba59d2b968f4388f
Bee Nguyễn <bee.nguyen@blockify.it>
2025-07-07 17:41:12 +0700

    up

--- src/pages/auth-callback.tsx
+++ src/pages/auth-callback.tsx
@@ -9,16 +9,17 @@ export default function AuthCallbackPage() {
 
   useEffect(() => {
     if (!router.isReady) return;
+    const pathname = localStorage.getItem('fallback-domain');
 
     const token = router.query.token;
 
     if (typeof token === 'string') {
-      const pathname = localStorage.getItem('fallback-domain');
 
       SetAuthToken(token);
       router.replace(String(pathname));
     } else {
       console.warn('No token found in query params.');
+      router.replace(String(pathname));
     }
   }, [router.isReady, router.query.token]);
 
